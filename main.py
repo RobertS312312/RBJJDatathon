a912010e56f227accb7178f2d13fa43b5d0bd709
Joshua Washington <josh8551021@gmail.com>
2022-01-29 00:03:18 -0600

    Add top row for title, logo

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ def setup_page():
   with cols[1]:
     st.title("Cognite Challenge")
 
+  cols = st.columns([1, 3, 1])
+  with cols[1]:
     # Add introductory information
     st.header("Introduction")
     st.write("Lorem ipsum dolor amet.")
 
     # Time series plot
-
   with cols[2]:
     option = st.selectbox("Weather station", ("KIKT", "KAPT", "KMIS"))
 
