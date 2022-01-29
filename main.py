76d993229ceb6c3e2ba8f595b2cd355bf7570102
rschelle <97462333+RobertS312312@users.noreply.github.com>
2022-01-29 10:00:30 -0600

    Coupled main.py and data_clean.py
    Set up main.py's file IO. Now reads in uncleaned. txt's and writes cleaned .csvs's using data_clean.py. I am also adding the resulting csv's so bohan knows what to expect in his r

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import streamlit as st
+import data_clean
+import os
 
+def UpdateData(strName, cached = True):
+
+  
+  if cached:
+    txt_filepath = os.path.join("UncleanedData",strName + ".txt")
+    csv_filepath = os.path.join("CleanedData",strName + ".csv")
+  else:
+    txt_filepath = os.path.join("UncleanedData", "newest" + strName + ".txt")
+    csv_filepath = os.path.join("CleanedData", "newest" + strName + ".csv")
+  
+
+  cleaned_df = data_clean.CleanWholeDataFrame(data_clean.filepath2df(txt_filepath))
+  cleaned_df.to_csv(csv_filepath)
+  ##do r process here
+  CreateVisuals(cleaned_df)
 
 def SetupPage():
   topbar = st.columns([1, 3, 1])
@@ -20,6 +37,18 @@ def SetupPage():
   data_plots = st.columns([1, 3, 1])
   with data_plots[2]:
     option = st.selectbox("Weather station", ("KIKT", "KAPT", "KMIS"))
+    UpdateData(option)
+
+def CreateVisuals(cleaned_df):
+  ##Create 1 helper function for each visual 
+  ##this function will call of those and impose the proper formatting
+  pass
+
+
+for station_name in ("KIKT", "KBQX", "KMIS"):
+  if station_name == "KBQX":
+    pass
+  UpdateData(station_name)
 
 
 if __name__ == "__main__":
