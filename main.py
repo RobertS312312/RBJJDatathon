2c9edbb5b1b871cebc3b6de773e30db2fb0c7e72
rschelle <97462333+RobertS312312@users.noreply.github.com>
2022-01-29 00:06:51 -0600

    Add logo to streamlit

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import streamlit as st
 def setup_page():
   cols = st.columns([1, 3, 1])
 
+  with cols[0]:
+    st.image("BakerBuoysLogo.png")
+
   with cols[1]:
     st.title("Cognite Challenge")
 
