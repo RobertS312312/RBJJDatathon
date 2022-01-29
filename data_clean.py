b300878a55d20f808b0d70825b8553d972e6d704
rschelle <97462333+RobertS312312@users.noreply.github.com>
2022-01-29 01:38:50 -0600

    Modularized data_clean.py
    Modularized data_clean.py and finished it so the main function to use is filepath2df(filepath)

--- data_clean.py
+++ data_clean.py
@@ -1,51 +1,83 @@
+import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+import io
 
-kbqx_data = pd.read_csv('UncleanedData\KBQX.csv')
-kikt_data = pd.read_csv('UncleanedData\KIKT.csv')
-kmis_data = pd.read_csv('UncleanedData\KMIS.csv')
 
-# kbqx_nan_count = kbqx_data.isnull().sum(axis = 0)
-# kikt_nan_count = kikt_data.isnull().sum(axis = 0)
-# kmis_nan_count = kmis_data.isnull().sum(axis = 0)
 
-drop_list = ['WVHT','DPD','GST','APD','MWD','PRES','WTMP','PTDY','TIDE']
+def SimpleInterpolateColumn(one_pandas_columns):
+    
+    for i in range(len(one_pandas_columns)):
+        if pd.isna(one_pandas_columns.iloc[i]):
+            # We know [i-1] must be non null at the point, only have 
+            # to worry about if [i+1] is non null, if it is null we go 
+            # forward until we reach end of data or we find one that is 
+            # non null
 
-kbqx_data.drop(drop_list, inplace=True, axis=1)
-kikt_data.drop(drop_list, inplace=True, axis=1)
-kmis_data.drop(drop_list, inplace=True, axis=1)
-        
-# kbqx_nan_count = kbqx_data.isnull().sum(axis = 0)
-# kikt_nan_count = kikt_data.isnull().sum(axis = 0)
-# kmis_nan_count = kmis_data.isnull().sum(axis = 0) 
+            next_non_null_idx = i+1
+            while True:
+                if next_non_null_idx < len(one_pandas_columns):
+                    #If there is no future, then just use the past
+                    one_pandas_columns.iloc[i] = one_pandas_columns.iloc[i-1]
+                    break
+                elif not pd.isna(one_pandas_columns[next_non_null_idx]):
+                    ##Then we have found the future
+                    one_pandas_columns.iloc[i] = one_pandas_columns.iloc[next_non_null_idx] + one_pandas_columns.iloc[i-1]
+                    break
+                else:
+                    next_non_null_idx+=1
+    
+    return one_pandas_columns
 
-for col in kbqx_data.columns:
-    if col == 'Date':
-        break
-    for i in range(len(kbqx_data)):
-        if np.isnan(kbqx_data[col][i]):
-            kbqx_data[col][i]= kbqx_data[col][i-1]
-            
-for col in kikt_data.columns:
-    if col == 'Date':
-        break
-    for i in range(len(kikt_data)):
-        if np.isnan(kikt_data[col][i]):
-            kikt_data[col][i]= kikt_data[col][i-1]
-            
-for col in kmis_data.columns:
-    if col == 'Date':
-        break
-    for i in range(len(kmis_data)):
-        if np.isnan(kmis_data[col][i]):
-            kmis_data[col][i]= kmis_data[col][i-1]
-            
-            
-kbqx_nan_count = kbqx_data.isnull().sum(axis = 0)
-kikt_nan_count = kikt_data.isnull().sum(axis = 0)
-kmis_nan_count = kmis_data.isnull().sum(axis = 0) 
+def CleanWholeDataFrame(df):
 
-kbqx_data.to_csv('CleanData\KBQX.csv')
-kikt_data.to_csv('CleanData\KIKT.csv')
-kmis_data.to_csv('CleanData\KMIS.csv')
+    df = df.drop(labels = 0, axis = 0)
+
+    ##These columns were 80+% nulls when we wrote the project
+    drop_list = ['WVHT','DPD','GST','APD','MWD','PRES','WTMP','PTDY','TIDE']
+
+    df.drop(drop_list, inplace=True, axis=1)
+
+    for col_name in df.columns:
+        df[col_name][ df[col_name] == "MM" ] = np.nan
+        df[col_name] = df[col_name].astype("float32")
+
+    for col in df.columns:
+        if col == 'Date':
+            break
+    else:
+        df[col] = SimpleInterpolateColumn(df[col])
+
+    return df
+
+def to_csv_string(filepath):
+
+    f = open(filepath)
+    csv_string = ""
+    for line in f:
+        csv_string += ",".join( line.split() )
+        csv_string += "\n"
+    
+    return csv_string.strip()
+
+def filepath2df(filepath):
+
+    stream = io.StringIO(to_csv_string(filepath))
+    df = pd.read_csv(stream, sep = ",")
+    return df
+
+
+#kbqx_data_old = pd.read_csv("UncleanedData\KBQX.csv")
+
+#kbqx_data_new = pd.read_table("UncleanedData\KBQX.txt")
+
+
+#kbqx_nan_count = kbqx_data.isnull().sum(axis = 0)
+#kikt_nan_count = kikt_data.isnull().sum(axis = 0)
+#kmis_nan_count = kmis_data.isnull().sum(axis = 0) 
+
+#kbqx_data.to_csv('CleanData\KBQX.csv')
+#kikt_data.to_csv('CleanData\KIKT.csv')
+#kmis_data.to_csv('CleanData\KMIS.csv')
